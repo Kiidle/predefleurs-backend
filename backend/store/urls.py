27ec4aa693682a571f123de9632d7ae046187849
Kiidle <jonas.buechi@4teamwork.ch>
2023-07-25 01:43:34 +0200

    Add Reservation System

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import ArticlesView, CreateArticleView, UpdateArticleView, DeleteArticleView
+from .views import ArticlesView, CreateArticleView, UpdateArticleView, DeleteArticleView, ReservationView, ArchivedReservationView
 
 urlpatterns = [
     path("", ArticlesView.as_view(), name="articles"),
@@ -8,4 +8,7 @@ urlpatterns = [
     path("article-<int:pk>/update", UpdateArticleView.as_view(), name="article_update"),
     path("article-<int:pk>/delete", DeleteArticleView.as_view(), name="article_delete"),
     path("article-<int:pk>/delete-confirmed", views.delete_article, name="article_delete_confirmed"),
+    path('article-<int:article_id>/reservating/', views.create_reservation, name='create_reservation'),
+    path("reservations/", ReservationView.as_view(), name="reservations"),
+    path("reservations/archive", ArchivedReservationView.as_view(), name="archived_reservations")
 ]
